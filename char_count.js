f3a3f015f7dc5a2de625f8fd21dea32fb8612cdb
alvannwanorim <alvannwanorim@gmail.com>
2022-03-12 22:12:11 +0100

    refactoring

--- char_count.js
+++ char_count.js
@@ -1,9 +1,18 @@
+function isAlphaNumeric(char) {
+    let code = char.charCodeAt(0)
+    if (!(code > 47 && code < 58) && !(code > 64 && code < 91) && !(code > 96 && code < 123)) {
+        return false
+    }
+    return true
+}
+
+
 function charCount(str) {
     let result = {}
     for (let i = 0; i < str.length; i++) {
         let char = str[i].toLowerCase()
 
-        if (/[a-z0-9]/.test(char)) {
+        if (isAlphaNumeric(char)) {
 
             result[char] ? result[char] += 1 : result[char] = 1
         }
