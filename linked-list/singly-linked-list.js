f0e2cc77dcaae25b37c0369025485cf2c0474e6c
alvannwanorim <alvannwanorim@gmail.com>
2022-04-21 00:00:51 +0100

    singly-linked-list cont'd

--- linked-list/singly-linked-list.js
+++ linked-list/singly-linked-list.js
@@ -13,16 +13,65 @@ class singlyLinkedList {
     }
 
     push(val) {
-        const node = new Node(val)
+        const newNode = new Node(val)
         if (!this.head) {
-            this.head = node
+            this.head = newNode
             this.tail = this.head
         } else {
-            this.tail.next = node
-            this.tail = node
+            this.tail.next = newNode
+            this.tail = newNode
         }
         this.length += 1
         return this
+    }
+    pop() {
+        if (!this.head) return undefined
+        var current = this.head
+        var newTail = this.head
+        while (current.head) {
+            current = current.next
+            newTail = current
+        }
+        this.tail = newTail
+        this.tail.next = null
+
+        this.length--
+
+        if (this.length === 0) {
+            this.head = null
+            this.tail = null
+        }
+        return newTail
+    }
+    shift() {
+        if (!this.head) return undefined
+        var currentHead = this.head
+        var nextHead = currentHead.next
+        this.head = nextHead
+        this.length--
+        if (this.length === 0) {
+            this.tail = null
+        }
+
+
+        return currentHead
+
+    }
+    unshift(val) {
+        const newNode = new Node(val)
+        if (!this.head) {
+            this.head = newNode
+            this.tail = this.head
+            this.length++
+            return this
+        }
+        var expiredHead = this.head
+        this.head = newNode
+        this.head.next = expiredHead
+        this.length++
+        return this
+
+
     }
 }
 
